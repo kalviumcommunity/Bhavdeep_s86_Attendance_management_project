904c7477c7f8592a41678d07582342cea258d7c9
Bhavdeep-Sai <bhavdeepsai@gmail.com>
2025-09-23 11:11:23 +0530

    feat: Implement Storable interface and FileStorageService for saving data (Part 6)

--- src/com/school/Course.java
+++ src/com/school/Course.java
@@ -1,6 +1,6 @@
 package com.school;
 
-public class Course {
+public class Course implements Storable {
     private static int nextCourseIdCounter = 101; // static counter for auto-ID
     private int courseId;
     private String courseName;
@@ -23,5 +23,10 @@ public class Course {
     // Display method
     public void display() {
         System.out.println("Course ID: C" + courseId + ", Name: " + courseName);
+    }
+
+    @Override
+    public String toDataString() {
+        return courseId + "," + courseName;
     }
 }
